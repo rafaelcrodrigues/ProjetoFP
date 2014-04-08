d083e101b59a93a6a7eae1d173f3af8609d88fc1
Lucaspadua <lspadua1@hotmail.com>
2014-04-07 21:24:41 -0300

    Listando Pessoas

--- pessoas/views.py
+++ pessoas/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from pessoas.models import Pessoa
 
-# Create your views here.
+def index(request):
+    return render(request, 'index.html')
+
+def pessoaListar(request):
+    pessoas = Pessoa.objects.all()[0:10]
+    #TESTE LOCAL PARA VERIFICAR SE A TABELA ESTA LISTANDO
+    #pessoas = []
+    #pessoas.append(Pessoa(nome='UNIFRAN', email='MAIL'))
+    return render(request, 'pessoas/listaPessoas.html', {'pessoas': pessoas})
+
+def pessoaAdicionar(request):
+    return render(request, 'pessoas/formPessoas.html')
+
